52a0d91a5c8d57872ba5d3df68aac00188a7a2da
Aidan Bailey <BLYAID001@myuct.ac.za>
2020-10-28 21:09:01 +0200

    added sorting todos

--- sorting.hpp
+++ sorting.hpp
@@ -1,5 +1,21 @@
 namespace sort {
 
-  //TODO
+// TODO BubbleSort
 
-}
+// TODO SelectionSort
+
+// TODO InsertionSort
+
+// TODO MergeSort
+
+// TODO QuickSort
+
+// TODO CountingSort
+
+// TODO RadixSort
+
+// TODO HeapSort
+
+// TODO BucketSort
+
+} // namespace sort
