c644a002fe78e3421fe101bc3e159cfa009c7e3e
Aidan Bailey <aidan.j.bailey@outlook.com>
2022-06-27 20:29:38 +0200

    Explicitly use search namespace

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,13 @@ pub mod search;
 
 #[cfg(test)]
 mod search_tests {
-    use crate::search::linear;
+    use crate::search;
 
     #[test]
     fn linear_search_i32_good() {
         let items = [0, -2, 5, 3, -10, 50, -12, 4, 200, -42];
         let item = 4;
-        let result = linear(&item, items.iter());
+        let result = search::linear(&item, items.iter());
         if let Some(index) = result {
             assert_eq!(index, 7)
         } else {
@@ -20,7 +20,7 @@ mod search_tests {
     fn linear_search_i32_bad() {
         let items = [0, -2, 5, 3, -10, 50, -12, 4, 200, -42];
         let item = -1;
-        let result = linear(&item, items.iter());
+        let result = search::linear(&item, items.iter());
         if let Some(_) = result {
             assert!(false)
         } else {
@@ -32,7 +32,7 @@ mod search_tests {
     fn linear_search_f32_good() {
         let items = [0.0, -2.5, 5.123, 3.10425, -10.213000, 50.22222333, -12.123123, 4.0425, 200.9952, -42.10042];
         let item = 4.0425;
-        let result = linear(&item, items.iter());
+        let result = search::linear(&item, items.iter());
         if let Some(index) = result {
             assert_eq!(index, 7)
         } else {
@@ -44,7 +44,7 @@ mod search_tests {
     fn linear_search_f32_bad() {
         let items = [0.0, -2.5, 5.123, 3.10425, -10.213000, 50.22222333, -12.123123, 4.0425, 200.9952, -42.10042];
         let item = 42.42;
-        let result = linear(&item, items.iter());
+        let result = search::linear(&item, items.iter());
         if let Some(_) = result {
             assert!(false)
         } else {
@@ -56,7 +56,7 @@ mod search_tests {
     fn linear_search_char_good() {
         let items = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
         let item = 'h';
-        let result = linear(&item, items.iter());
+        let result = search::linear(&item, items.iter());
         if let Some(index) = result {
             assert_eq!(index, 7)
         } else {
@@ -68,7 +68,7 @@ mod search_tests {
     fn linear_search_char_bad() {
         let items = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
         let item = 'k';
-        let result = linear(&item, items.iter());
+        let result = search::linear(&item, items.iter());
         if let Some(_) = result {
             assert!(false)
         } else {
@@ -80,7 +80,7 @@ mod search_tests {
     fn linear_search_str_good() {
         let items = ["ab", "bc", "cd", "de", "ef", "fg", "gh", "hi", "ij", "jk"];
         let item = "hi";
-        let result = linear(&item, items.iter());
+        let result = search::linear(&item, items.iter());
         if let Some(index) = result {
             assert_eq!(index, 7)
         } else {
@@ -92,7 +92,7 @@ mod search_tests {
     fn linear_search_str_bad() {
         let items = ["ab", "bc", "cd", "de", "ef", "fg", "gh", "hi", "ij", "jk"];
         let item = "kl";
-        let result = linear(&item, items.iter());
+        let result = search::linear(&item, items.iter());
         if let Some(_) = result {
             assert!(false)
         } else {
