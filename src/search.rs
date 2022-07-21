0f3d03232fd4d24bf1c72345b917900c834e4ae5
Aidan Bailey <aidan.j.bailey@outlook.com>
2022-07-21 21:15:57 +0200

    Add binary and linear search

--- src/search.rs
+++ src/search.rs
@@ -1,10 +1,49 @@
-pub fn linear<T: std::cmp::PartialEq>(
-    item: &T,
-    items: &Vec<T>,
-) -> Option<usize> {
+pub fn linear<T: std::cmp::PartialEq>(item: &T, items: &Vec<T>) -> Option<usize> {
     for (index, value) in items.iter().enumerate() {
         if item == value {
             return Some(index);
+        }
+    }
+    return None;
+}
+
+pub fn binary<T: std::cmp::PartialOrd>(item: &T, items: &Vec<T>) -> Option<usize> {
+    let mut l: usize = 0;
+    let mut r: usize = items.len();
+    while l <= r {
+        let m: usize = (l + r) / 2;
+        if &items[m] < item {
+            l = m + 1;
+        } else if &items[m] > item {
+            r = m - 1;
+        } else {
+            return Some(m);
+        }
+    }
+    return None;
+}
+
+pub fn ternary<T: std::cmp::PartialOrd>(item: &T, items: &Vec<T>) -> Option<usize> {
+    let mut l: usize = 0;
+    let mut r: usize = items.len();
+    while l <= r {
+        let m1: usize = l + (r - l) / 3;
+        let m2: usize = r - (r - l) / 3;
+        if &items[m1] == item {
+            return Some(m1)
+        }
+        else if &items[m2] == item {
+            return Some(m2)
+        }
+        else if &items[m2] < item {
+            l = m2 + 1;
+        }
+        else if &items[m1] < item {
+            l = m1 + 1;
+            r = m2 - 1;
+        }
+        else {
+            r = m1 - 1;
         }
     }
     return None;
