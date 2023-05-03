90edd00582f5c52c97ae12ff6a80491fe554b3c6
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-05-03 20:39:34 +0200

    Doc linear

--- CSAlgos/CSAlgos/Searching.cs
+++ CSAlgos/CSAlgos/Searching.cs
@@ -1,6 +1,13 @@
 ﻿namespace CSAlgos;
 public static class Searching
 {
+    /**
+     * Linear Search
+     *
+     * @param items a list of items to search
+     * @param item an item to search for
+     * @return the index of the item if found, null if not
+     */
     public static int? Linear<T>(List<T> items, T item) where T : IEquatable<T> {
         for (int i = 0; i < items.Count; i++)
             if (items[i].Equals(item))
