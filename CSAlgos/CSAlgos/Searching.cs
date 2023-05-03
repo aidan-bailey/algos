9e00fb50247ac8e501bf8864f12594a9c23b1c3d
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-05-03 20:40:13 +0200

    Reformat

--- CSAlgos/CSAlgos/Searching.cs
+++ CSAlgos/CSAlgos/Searching.cs
@@ -8,10 +8,11 @@ public static class Searching
      * @param item an item to search for
      * @return the index of the item if found, null if not
      */
-    public static int? Linear<T>(List<T> items, T item) where T : IEquatable<T> {
+    public static int? Linear<T>(List<T> items, T item) where T : IEquatable<T>
+    {
         for (int i = 0; i < items.Count; i++)
             if (items[i].Equals(item))
                 return i;
         return null;
-    } 
+    }
 }
