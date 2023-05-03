9ac56678a1b9ea125439453d7b01a02cd11338aa
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-05-03 20:37:14 +0200

    Add tests for linear

--- CSAlgos/CSAlgosTest/SearchingTest.cs
+++ CSAlgos/CSAlgosTest/SearchingTest.cs
@@ -7,13 +7,14 @@ public class SearchingTests
 
     private static List<int> items = TestUtils.GenerateList(1000, false);
 
-    public void SearchTemplate(Func<List<int>, int?> func) {
-
+    public void SearchTemplate(Func<List<int>, int, int?> func)
+    {
+        Assert.AreEqual(func(items, items[0]), 0);
+        Assert.AreEqual(func(items, items[items.Count - 1]), items.Count - 1);
+        Assert.AreEqual(func(items, items[500]), 500);
+        Assert.AreEqual(func(items, -1), null);
     }
 
     [TestMethod]
-    public void LinearTest()
-    {
-        Assert.AreEqual(Searching.Linear(new List<int>{1, 2, 3}, 1), 0);
-    }
+    public void LinearTest() => SearchTemplate(Searching.Linear<int>);
 }
