7127c0e7761db8f6386bcef5e2e7263a4321efb9
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-05-25 23:12:19 +0200

    Format

--- JAlgos/lib/src/main/java/jalgos/Searching.java
+++ JAlgos/lib/src/main/java/jalgos/Searching.java
@@ -4,19 +4,18 @@ import java.util.List;
 import java.util.Optional;
 
 public class Searching {
-    
-    
+
     /**
      * Linear Search
      * 
-     * @param <T>       the type of item
-     * @param items     the list of items to be search
-     * @param item      the item to search for
-     * @return          index of item if found, empty if not
+     * @param <T>   the type of item
+     * @param items the list of items to be search
+     * @param item  the item to search for
+     * @return index of item if found, empty if not
      */
-    public static <T extends Comparable<T>> Optional<Integer> Linear(List<T> items, T item){
+    public static <T extends Comparable<T>> Optional<Integer> Linear(List<T> items, T item) {
         for (int i = 0; i < items.size(); i++) {
-            if (items.get(i).equals(item)){
+            if (items.get(i).equals(item)) {
                 return Optional.of(i);
             }
         }
