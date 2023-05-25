6469a4fe93e22fd501af5a18fadfa4bf60ecbea8
Aidan Bailey <aidan.j.bailey@outlook.com>
2023-05-25 22:16:48 +0200

    Add Java skeleton classes

---
+++ JAlgos/lib/src/main/java/jalgos/Sorting.java
@@ -0,0 +1,5 @@
+package jalgos;
+
+public class Sorting {
+
+}
---
+++ JAlgos/lib/src/test/java/jalgos/SearchingTest.java
@@ -0,0 +1,11 @@
+package jalgos;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SearchingTest {
+    @Test void someLibraryMethodReturnsTrue() {
+        Sorting classUnderTest = new Sorting();
+        assertTrue(true, "someLibraryMethod should return 'true'");
+    }
+}
